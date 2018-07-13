1231cd2808161c9b48f8cd50deb67ed5c3cc9558
Priyansh Sangule <priyansh0506@gmail.com>
2018-07-13 10:10:50 +0530

    Add files via upload

--- Tirutsava-Website/README.md
+++ Tirutsava-Website/README.md
@@ -1,4 +1,4 @@
-# TirutsavaWebsite
+# Counter
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- Tirutsava-Website/src/app/app.module.ts
+++ Tirutsava-Website/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
