dd6a4e01dadf1ee33d4ab7b321ea64e598021736
RohithS98 <ee16b025@iittp.ac.in>
2018-11-11 18:07:16 +0530

    Updated archive with smaller images

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ArchiveComponent implements OnInit {
   prefix = "../../assets/archive/";
   pictures = [
-      "ps1.jpg", "ps2.jpg", "ps3.jpg", "ps4.jpg",
-      "ps5.jpg", "ps6.jpg", "ps7.jpg", "ps8.jpg",
-      "ps9.jpg", "ps10.jpg", "ps11.jpg", "ps12.jpg",
+      "p1.jpg", "p2.jpg", "p3.jpg", "p4.jpg",
+      "p5.jpg", "p6.jpg", "p7.jpg", "p8.jpg",
+      "p9.jpg", "p10.jpg", "p11.jpg", "p12.jpg",
+      "p13.jpg", "p14.jpg", "p15.jpg", "p16.jpg",
+      "p17.jpg", "p18.jpg", "p19.jpg", "p20.jpg",
+      "p21.jpg", "p22.jpg", "p23.jpg", "p24.jpg",
+      "p25.jpg", "p26.jpg", "p27.jpg", "p28.jpg",
+      "p29.jpg", "p30.jpg", "p31.jpg", "p32.jpg",
+      "p33.jpg",
   ];
 
   constructor() {
