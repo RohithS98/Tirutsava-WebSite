fdee9ec1621f3c529003c9fe7c9fae43dad8c0e2
RohithS98 <ee16b025@iittp.ac.in>
2018-11-12 18:37:35 +0530

    Compressed images further

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -8,15 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ArchiveComponent implements OnInit {
   prefix = "../../assets/archive/";
   pictures = [
-      "p1.jpg", "p2.jpg", "p3.jpg", "p4.jpg",
-      "p5.jpg", "p6.jpg", "p7.jpg", "p8.jpg",
-      "p9.jpg", "p10.jpg", "p11.jpg", "p12.jpg",
-      "p13.jpg", "p14.jpg", "p15.jpg", "p16.jpg",
-      "p17.jpg", "p18.jpg", "p19.jpg", "p20.jpg",
-      "p21.jpg", "p22.jpg", "p23.jpg", "p24.jpg",
-      "p25.jpg", "p26.jpg", "p27.jpg", "p28.jpg",
-      "p29.jpg", "p30.jpg", "p31.jpg", "p32.jpg",
-      "p33.jpg",
+      "1.jpg", "2.jpg", "3.jpg", "4.jpg",
+      "5.jpg", "6.jpg", "7.jpg", "8.jpg",
+      "9.jpg", "10.jpg", "11.jpg", "12.jpg",
+      "13.jpg", "14.jpg", "15.jpg", "16.jpg",
+      "17.jpg", "18.jpg", "19.jpg", "20.jpg",
   ];
 
   constructor() {
