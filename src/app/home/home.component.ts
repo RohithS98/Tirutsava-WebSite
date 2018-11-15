f3fc5ef91ace042729fa84c2e0a9738744c88395
RohithS98 <ee16b025@iittp.ac.in>
2018-11-16 00:59:06 +0530

    Clean Up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,32 +9,6 @@ import * as anime from 'animejs';
 
 export class HomeComponent implements OnInit {
 
-//   animatedLogo = anime.timeline({
-//     autoplay: false,
-//   }).add({
-//     targets: '#logo',
-//     scale: ['0.01','1'],
-//     rotate: ['0.2turn','1turn'],
-//     easing: 'easeOutElastic',
-//     elasticity:'100',
-//     duration: 3500,
-//     offset: 0,
-//   }).add({
-//     targets: "#linelogo .a",
-//     stroke: '#fe8d07',
-//     strokeDashoffset: [anime.setDashoffset, 0],
-//     easing: 'easeInOutSine',
-//     duration: 1200,
-//     offset : 0,
-//   }).add({
-//     targets: "#linelogo .a1",
-//     stroke: '#fe8d07',
-//     strokeDashoffset: [anime.setDashoffset, 0],
-//     easing: 'easeInOutSine',
-//     duration: 1000,
-//     offset : 0,
-// });
-
   constructor() { }
 
   ngOnInit() {
