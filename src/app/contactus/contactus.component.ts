4fe49fd0ad0f06d538134afe1349a4f042e3217c
RohithS98 <ee16b025@iittp.ac.in>
2018-11-18 21:32:33 +0530

    Added load animation to contact us

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import * as anime from 'animejs';
 
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css']
 })
+
 export class ContactusComponent implements OnInit {
 
   constructor() { }
@@ -14,7 +16,16 @@ export class ContactusComponent implements OnInit {
   ngOnInit() {
   }
 
-  emailFormControl = new FormControl('',[Validators.required,Validators.email]);
-  requiredFormControl = new FormControl('',[Validators.required]);
-
+  ngAfterViewInit() {
+    anime({
+      targets:".icon",
+      translateY: ['500', '0'],
+      rotate:['0.5turn','0turn'],
+      delay: function(el,i,l){
+        return 100 + i*100;
+      },
+      duration: 1000,
+      scale: ['0.05','1'],
+    });
+  }
 }
