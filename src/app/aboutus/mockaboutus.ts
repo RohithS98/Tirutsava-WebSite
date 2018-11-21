ea386fd114aa8ed2409eca3aad01abd52a26eda0
RohithS98 <ee16b025@iittp.ac.in>
2018-11-21 18:25:19 +0530

    Added footer and images

--- src/app/aboutus/mockaboutus.ts
+++ src/app/aboutus/mockaboutus.ts
@@ -27,15 +27,15 @@ export const Teams = [
         new Person('Harthik Rao', 'Head', '../../assets/aboutus/Harthik.jpg' , 'https://www.facebook.com/miriyala.harthikrao' , ' ' , '9454197941'),
     ]),
     new Team('Finance Team', [
-        new Person('Leela Krishna Mohan', 'Head', '', 'https://www.facebook.com/sai.lkm', '', '7680872068'),
+        new Person('Leela Krishna Mohan', 'Head', '../../assets/aboutus/Leela.jpg', 'https://www.facebook.com/sai.lkm', '', '7680872068'),
     ]),
     new Team('Marketing Team', [
-        new Person('Pratyusha Vudutala', 'Head', '', 'https://www.facebook.com/Pratyusha.Vudutala', '', '8008987552'),
+        new Person('Pratyusha Vudutala', 'Head', '../../assets/aboutus/PV.jpg', 'https://www.facebook.com/Pratyusha.Vudutala', '', '8008987552'),
         new Person('Parth Dhabalia', 'Head', '../../assets/aboutus/Parth.jpg', 'https://www.facebook.com/parth.dhabalia.7', '', '9426944073'),
     ]),
     new Team('Creative Team', [
         new Person('Deeksha Reddy', 'Head', '', '', '', '9121804674'),
-        new Person('Kiran Jyothi Guntur', 'Head', '', 'https://www.facebook.com/kiranjyothi.guntur', '', '7680951210'),
+        new Person('Kiran Jyothi Guntur', 'Head', '../../assets/aboutus/Kiran.jpg', 'https://www.facebook.com/kiranjyothi.guntur', '', '7680951210'),
     ]),
     new Team('Web Design Team', [
         new Person('Priyansh Sangule', 'Head', '', 'https://www.facebook.com/abstractxan', '', '9165840183'),
@@ -50,10 +50,10 @@ export const Teams = [
         new Person('Keerthi Vardhan', 'Head', '../../assets/aboutus/Bro.jpg', 'https://www.facebook.com/keerthi.vardhan.505', '', '7995855703'),
     ]),
     new Team('Technical Team', [
-        new Person('Vishnudev Choudhary', 'Head', '../../assets/aboutus/Vishnu.jpg', 'https://www.facebook.com/profile.php?id=100003207622578', '', '7023232681'),
+        new Person('Vishnudev Choudhary', 'Head', '../../assets/aboutus/Vishnudev.jpg', 'https://www.facebook.com/profile.php?id=100003207622578', '', '7023232681'),
     ]),
     new Team('Hospitality Team', [
         new Person('Atharva Gupta', 'Head', '../../assets/aboutus/Atharva.jpg', 'https://m.facebook.com/Rumo.CS', '', '8871313300'),
-        new Person('Mahesh Vidala', 'Head', '', 'https://www.facebook.com/maheshkumar.vidala', '', '8074683254'),
+        new Person('Mahesh Vidala', 'Head', '../../assets/aboutus/Mahesh.jpg', 'https://www.facebook.com/maheshkumar.vidala', '', '8074683254'),
     ]),
 ];
