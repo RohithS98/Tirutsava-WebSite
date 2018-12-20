45f36fb4a86c13e83288d3e1d573855b73f33aee
Priyansh Sangule <priyansh0506@gmail.com>
2018-12-20 16:33:34 +0530

    Events Page

--- src/app/events/mockevents.ts
+++ src/app/events/mockevents.ts
@@ -4,22 +4,25 @@ export const CultEvent = 2;
 // To be exported and used
 export const EventType = TechEvent | CultEvent ;
 
-export class Event{
+export class Event {
     constructor(
         public name: string,
+        public desc: string,
+        public rounds_desc: string,
+        public rules: string,
+        public fee: string,
         public type: any= EventType,
-    ){}
+    ) {}
 }
 
 export const mockTechEvents = [
-    new Event('Respawn',TechEvent),
-    new Event('Enigma',TechEvent),
-]
+    new Event('Respawn', ' ', ' ', ' ', ' ', TechEvent),
+];
 
 export const mockCultEvents = [
-    new Event('Dance',CultEvent),
-    new Event('Drama',CultEvent)
-]
+    // new Event('Dance',CultEvent),
+    // new Event('Drama',CultEvent)
+];
 
 //Export mockevents
 export const mockEvents = mockTechEvents.concat(mockCultEvents);
